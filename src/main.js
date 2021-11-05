da2af070bee91b0fc57e96d0dbe30fe83fbc7420
kinoko109 <tatsuaki1009@gmail.com>
2021-11-05 23:18:26 +0900

    update: interceptors実装

--- src/main.js
+++ src/main.js
@@ -9,7 +9,48 @@ axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-axi
 // axios.defaults.headers.common["Authorization"] = "common";
 // axios.defaults.headers.get["Authorization"] = "get";
 
+// interceptorsは複数設定できる
+// リクエストする前になにか処理をしたい
+// id: 0
+axios.interceptors.request.use(
+    config => {
+        console.log("interceptors request", config);
+        return config;
+    },
+    error => {
+        console.log("interceptors request", error);
+        return Promise.reject(error);
+    }
+)
+
+// id: 1
+axios.interceptors.request.use(
+    config => {
+        console.log("interceptors request", config);
+        return config;
+    },
+    error => {
+        console.log("interceptors request", error);
+        return Promise.reject(error);
+    }
+)
+
+// レスポンスを受ける前になにか処理をしたい
+axios.interceptors.response.use(
+    config => {
+        console.log("interceptors response", config);
+        return config;
+    },
+    error => {
+        console.log("interceptors response", error);
+        return Promise.reject(error);
+    }
+)
+
+// 指定したidのinterceptorsを取り消す
+axios.interceptors.request.eject(0)
+
 new Vue({
-  render: h => h(App),
-  store,
+    render: h => h(App),
+    store,
 }).$mount('#app')
