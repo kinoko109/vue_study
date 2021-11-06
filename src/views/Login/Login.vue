9a4a4e0652751966ca4730387c607ee14244c79f
kinoko109 <tatsuaki1009@gmail.com>
2021-11-06 14:47:38 +0900

    update: ログイン機能

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -5,13 +5,15 @@
     <input id="email" v-model="email" type="text"/>
     <br><br>
     <label for="password">パスワード：</label>
-    <input id="password" v-model="comment" type="text" />
+    <input id="password" v-model="password" type="password"/>
     <br>
     <button @click="login">ログイン</button>
   </div>
 </template>
 
 <script>
+import axiosForLogin from "../../axios-auth";
+
 export default {
   data() {
     return {
@@ -21,7 +23,17 @@ export default {
   },
   methods: {
     login() {
-
+      axiosForLogin.post(`/accounts:signInWithPassword?key=${process.env.VUE_APP_API_KEY}`,
+          {
+            email: this.email,
+            password: this.password,
+            returnSecureToken: true
+          }
+      ).then(response => {
+        console.log(response);
+      })
+      this.email = "";
+      this.password = "";
     }
   }
 }
